59b45513f1d295b40b60dbafe6ca67ab5612497e
AntoniaGranit <antonia.granit@gmail.com>
2023-09-13 13:25:48 +0300

    change route names

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView.vue';
+import CostaCalida from '../views/CostaCalida.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,11 +12,11 @@ const router = createRouter({
     },
     {
       path: '/costa-calida',
-      name: 'costa-calida',
+      name: 'CostaCalida',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/costa-calida.vue')
+      component: CostaCalida
     }
   ]
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 
 <template>
   <main>
-    Hoooho!
+    Home
   </main>
 </template>
 
