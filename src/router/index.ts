02b77a08e0aaf5a803ca5c2cd87fa3ef01173c53
AntoniaGranit <antonia.granit@gmail.com>
2023-09-13 14:05:31 +0300

    correct index route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue';
-import CostaCalida from '../views/CostaCalida.vue';
+import CostaCalidaView from '../views/CostaCalidaView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,11 +12,11 @@ const router = createRouter({
     },
     {
       path: '/costa-calida',
-      name: 'CostaCalida',
+      name: 'CostaCalidaView',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: CostaCalida
+      component: CostaCalidaView
     }
   ]
 })
